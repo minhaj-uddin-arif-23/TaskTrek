6bbf1050f41c9addccf6304d9aa0a783e0713cdd
unknown <arifk62901@gmail.com>
2025-02-20 19:32:59 +0600

    task added to database

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,90 @@
-import { Outlet } from "react-router"
+import React from 'react'
+import banner from '/banner.jpg'
+import Swal from 'sweetalert2'
+export default function App() {
 
+    const formHandle = (e) => {
+        e.preventDefault()
+        const title = e.target.ttile.value.trim()
+        const details = e.target.details.value.trim()
+        const status = e.target.status.value.trim()
+        const time = new Date().toISOString();
+        const taskInfo = {title,details,status,time}
+        
+        if(title.length > 50){
+            Swal.fire({
+                title: "Error!",
+                icon: "error",
+                draggable: true
+              });
+              return;
+        }
+        if(details.length > 200) {
+            Swal.fire({
+                title: "Error!",
+                icon: "error",
+                draggable: true
+              });
+              return;
+        }
+        // console.log(taskInfo)
+        fetch(`http://localhost:8000/addTask`, {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(taskInfo),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+        
 
-function App() {
-  
+    }
 
-  return (
-   <div>
-     <div>
-      <aside class="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto  border-r rtl:border-r-0 rtl:border-l text-black dark:border-gray-700">
-    <a href="#" class="mx-auto">
-        <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
-    </a>
+    return (
+        <div>
 
-    <div class="flex flex-col items-center mt-6 -mx-2">
-        <img class="object-cover w-24 h-24 mx-2 rounded-full" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar" />
-        <h4 class="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">John Doe</h4>
-        <p class="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">john@example.com</p>
-    </div>
-
-    <div class="flex flex-col justify-between flex-1 mt-6">
-        <nav>
-            <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-800 dark:text-gray-200" href="#">
-               
-
-                <span class="mx-4 font-medium">Dashboard</span>
-            </a>
-
-            <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-               
-
-                <span class="mx-4 font-medium">Accounts</span>
-            </a>
-
-            <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                
-
-                <span class="mx-4 font-medium">Tickets</span>
-            </a>
-
-            <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-               
-
-                <span class="mx-4 font-medium">Settings</span>
-            </a>
-        </nav>
-    </div>
-</aside>
-    </div>
-    <div>
-      <Outlet />
-    </div>
-   </div>
+     <div className='flex items-center justify-center mb-5'>
+     <img src={banner} className='h-96 flex items-center' alt="hpw" />
+     </div>
+            {/* task */}
+            <div className="w-11/12 max-w-md mx-auto py-8 bg-white shadow-lg rounded-lg p-6">
+    <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">
+      Add Task and Track Your Journey
+    </h1>
+    <form className="space-y-4" onSubmit={formHandle}>
+      <input
+        type="text"
+        placeholder="Type your task"
+        name="ttile"
+        required
+        className="input input-bordered input-info w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <textarea
+        name="details"
+        placeholder="Task Description"
+        className="textarea textarea-bordered w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        rows="4"
+      ></textarea>
+    
+      <select
+        name="status"
+        className="select select-bordered w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        <option value="to-do">To Do</option>
+        <option value="in-progress">In Progress</option>
+        <option value="done">Done</option>
+      </select>
+      <p 
+      className="text-sm text-gray-500">Timestamp: {new Date().toLocaleString()}</p>
+      <button
+        type="submit"
+        className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-lg transition duration-300"
+      >
+        Add Task
+      </button>
+    </form>
+  </div>
+        </div>
   )
 }
-
-export default App
